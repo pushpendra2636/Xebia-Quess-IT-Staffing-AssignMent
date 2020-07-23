dc9931e94a412f3677e662693010d57368bfa4ac
pushpendra2636 <36572899+pushpendra2636@users.noreply.github.com>
2020-07-24 01:01:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Content from './components/Content';
+
+function App () {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+}
+
+export default App;
